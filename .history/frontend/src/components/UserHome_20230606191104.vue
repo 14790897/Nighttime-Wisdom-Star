b956e15cfd2582ee58ade7c0c99bfbdea0a5ef54
14790897 <14790897abc@gmail.com>
2023-06-06 19:13:35 +0800

    socketio-bug

--- .history/frontend/src/components/UserHome_20230606191104.vue
+++ .history/frontend/src/components/UserHome_20230606191104.vue
@@ -113,8 +113,8 @@
     created() {
       // this.socket = io.connect('https://flaskcloud.liuweiqing.top/', {withCredentials: true});
       if (!this.$socket) {
-        this.$socket = io('/', { withCredentials: true });
-      }
+    this.$socket = io('/', { withCredentials: true });
+  }
       this.socket.on('login_success',  () => {
         // After receiving the 'login_success' event, emit a 'join' event with the username.
         this.socket.emit('join');
