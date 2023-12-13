c5a64a6acdd2af78b7682b07a764bb511ea2e1ad
Liuweiqing <121866954+14790897@users.noreply.github.com>
2023-12-13 16:28:26 +0800

    fix:login failed ui

--- frontend/src/components/UserLogin.vue
+++ frontend/src/components/UserLogin.vue
@@ -1,59 +1,67 @@
 <template>
-  <div class="container">
-      <div class="row justify-content-center">
-          <div class="col-md-6">
-              <div class="card border-primary my-5">
-                <h2 class="mb-3">{{ $t('message.login') }}</h2>
-                <div class="card-body">
-                      <form @submit.prevent="submitForm">
-                          <div class="form-group">
-                              <label for="username">Username</label>
-                              <input id="username" v-model="username" type="text" class="form-control" placeholder="Username">
-                          </div>
-                          <div class="form-group">
-                              <label for="password">Password</label>
-                              <input id="password" v-model="password" type="password" class="form-control" placeholder="Password">
-                          </div>
-                          <button type="submit" class="btn btn-primary w-100">{{ $t('message.login') }}</button>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card border-primary my-5">
+                    <h2 class="mb-3">{{ $t('message.login') }}</h2>
+                    <div class="card-body">
+                        <form @submit.prevent="submitForm">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" v-model="username" type="text" class="form-control"
+                                    placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" v-model="password" type="password" class="form-control"
+                                    placeholder="Password">
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100">{{ $t('message.login') }}</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex';
 
 export default {
-  data() {
-      return {
-          username: '',
-          password: ''
-      }
-  },
-  methods: {
-      ...mapMutations(['setUsername']),
-      async submitForm() {
-          let response = await this.$http.post('/api/login', {
-              username: this.username,
-              password: this.password
-          });
-          if (response.data.status === 'success') {
-              // 使用 mutation 设置全局的 username
-              this.setUsername(this.username);
-              this.$router.push({name: 'Home'});
-          } else {
-              alert(this.$t('message.loginFailed'));
-          }
-      }
-  }
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        ...mapMutations(['setUsername']),
+        async submitForm() {
+            try {
+                let response = await this.$http.post('/api/login', {
+                    username: this.username,
+                    password: this.password
+                });
+                if (response.data.status === 'success') {
+                    this.setUsername(this.username);
+                    this.$router.push({ name: 'Home' });
+                } else {
+                    // 显示登录失败的原因
+                    alert(this.$t('message.loginFailed') + ': ' + response.data.message);
+                }
+            } catch (error) {
+                // 在这里处理错误，显示错误信息
+                let errorMessage = error.response && error.response.data ? error.response.data.message : error.message;
+                alert(this.$t('message.loginFailed') + ': ' + errorMessage);
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
 .container {
-  margin-top: 100px;
+    margin-top: 100px;
 }
 </style>
